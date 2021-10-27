e4efc152350ef46bd184daed7538611855eba9e6
seunghyo <tmdgy6232@naver.com>
2021-10-27 20:29:56 +0900

    Detect 모델 추가

--- detectapp/models.py
+++ detectapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Detect(models.Model):
+    classification_type = models.CharField(max_length=100, null=False)
+    image = models.ImageField(upload_to='detect/', null=False)
+    created_at = models.DateField(auto_now_add=True, null=False)
